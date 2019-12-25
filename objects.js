3835b588ce73ba6109d53b2460bb5a367d921617
Tristan <tristanelliott7@gmail.com>
2019-12-25 10:39:49 -0800

    deleting some console.logs that I used for debugging puerposes

--- objects.js
+++ objects.js
@@ -41,13 +41,12 @@ function createMountain(e){
 function createElement(...input){
 // this function will create one table row and will append table data to it, depending on ...input
 	let tr = document.createElement("tr"); // creates a new table row. only needs to be created once
-	console.log('the tr has been created');
 	for (let i of input){
-		console.log('inside the for loop')
 		let tdName1 =document.createElement("td"); // creates a table data
 		tdName1.appendChild(document.createTextNode(i.value)); // creates can appends the value to the table data
 		tr.appendChild(tdName1); // appends the created table data to the table row
 		tableBody.appendChild(tr); // will append the table row to the table row
+		i.value = ''// this should reset all of the values in the form
 
 	}
 }
